ff22f0595874608b2337279d63ef2bdc38cc84c1
Kadu_freitas Eduardo <Kadu_freitas Eduardo>
2017-04-03 12:26:26 -0300

    Criada navegacao lateral das paginas. close #1

--- public/components/controller/menu.controller.js
+++ public/components/controller/menu.controller.js
@@ -2,11 +2,16 @@
 
     var myAppModule = angular.module('MyApp');
 
-    var MenuController = function ($scope, HttpService) {
+    var MenuController = function ($scope, $state, HttpService) {
 
         $scope.tasks = [{
                 text: 'Create issues in github',
                 done: false
+                /*date: "01/04/2017",
+                tag: "work"*/
+        },{
+                text: 'Create GC.doc in github',
+                done: true
                 /*date: "01/04/2017",
                 tag: "work"*/
         }];
@@ -19,6 +24,16 @@
 
         }
 
+        $scope.showMade = function () {
+            console.log("Entro no Made");
+            $state.go('made');
+        };
+
+        $scope.showToDo = function () {
+            console.log("Entro no toDo");
+            $state.go('index');  
+        };
+
         $scope.showTasks = function () {
             $scope.flag = !$scope.flag;
         }
@@ -29,7 +44,7 @@
 
     };
 
-    MenuController.$inject = ['$scope','HttpService'];
+    MenuController.$inject = ['$scope', '$state','HttpService'];
     myAppModule.controller('MenuController',MenuController);
 
 }());
